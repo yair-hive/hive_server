549f3805df7902c56d11106fd3d9ad998cd21f02
yair-hive <yairman2429@gmail.com>
2023-09-01 03:22:16 +0300

    pp

--- main.js
+++ main.js
@@ -1,62 +1,62 @@
-/* eslint-disable @typescript-eslint/no-shadow */
-/* eslint-disable no-console */
+// /* eslint-disable @typescript-eslint/no-shadow */
+// /* eslint-disable no-console */
 import express from "express";
-import bodyParser from "body-parser";
-import session from "express-session";
-import expressMysqlSession from "express-mysql-session";
+// import bodyParser from "body-parser";
+// import session from "express-session";
+// import expressMysqlSession from "express-mysql-session";
 
-import projects from "./routs/api/projects.js";
-import actionsRouter from "./routs/actions.js";
-import api from "./routs/api.js";
-import wss from "./socket.js";
+// import projects from "./routs/api/projects.js";
+// import actionsRouter from "./routs/actions.js";
+// import api from "./routs/api.js";
+// import wss from "./socket.js";
 
-const MySQLStore = expressMysqlSession(session);
+// const MySQLStore = expressMysqlSession(session);
 
 const app = express();
 const port = 3025;
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
+// app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", req.get("origin"));
-  res.set("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", req.get("origin"));
+//   res.set("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.get("/", (req, res) => {
-  res.send("hello world 770");
+  res.send("לך לישון!!!!!!!!!!!!!! 🤦‍♂️🤦‍♀️🤦‍♀️🙌🙌👍🤦‍♂️👍🤦‍♀️");
 });
-app.get("/test", async (req, res) => {
-  const maps = await projects.export(req.query);
-  res.send(maps);
-});
-app.use("/actions", actionsRouter);
-app.use("/api", api);
+// app.get("/test", async (req, res) => {
+//   const maps = await projects.export(req.query);
+//   res.send(maps);
+// });
+// app.use("/actions", actionsRouter);
+// app.use("/api", api);
 
-const options = {
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hive",
-};
+// const options = {
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "hive",
+// };
 
-const sessionStore = new MySQLStore(options);
+// const sessionStore = new MySQLStore(options);
 
-app.use(
-  session({
-    store: sessionStore,
-    secret: "yair",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     store: sessionStore,
+//     secret: "yair",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 const server = app.listen(port, () => {
   console.log(`express start on port ${port}`);
 });
 
-server.on("upgrade", (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (socket) => {
-    wss.emit("connection", socket, request);
-  });
-});
+// server.on("upgrade", (request, socket, head) => {
+//   wss.handleUpgrade(request, socket, head, (socket) => {
+//     wss.emit("connection", socket, request);
+//   });
+// });
